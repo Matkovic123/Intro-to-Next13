09c2e7bbc92d66da0eb2e8a24da34b0fbf61ff77
Toni Matkovic <toni.matkovic0@gmail.com>
2023-03-13 18:23:26 +0100

    next router

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,10 +1,15 @@
+'use client'
+// Next components are server side by default
 import Link from "next/link"
+import {useRouter } from 'next/navigation';
 
 
 export default function BlogPost() {
+    const router = useRouter();
     return <div>
         <h1>Contact landing page</h1>
         <Link href="/">Home</Link>
         <Link href={`/blog/whateverVariable`}>Blog</Link>
+        <button onClick={() => router.push("/")}>home</button>
         </div>
 }

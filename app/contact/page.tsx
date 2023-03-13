21bd6ce40fac5814537a77a769ca500d86b4ae05
Toni Matkovic <toni.matkovic0@gmail.com>
2023-03-13 18:10:21 +0100

    Using Next Link component

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,3 +1,10 @@
+import Link from "next/link"
+
+
 export default function BlogPost() {
-    return <div>Contact landing page</div>
+    return <div>
+        <h1>Contact landing page</h1>
+        <Link href="/">Home</Link>
+        <Link href={`/blog/whateverVariable`}>Blog</Link>
+        </div>
 }

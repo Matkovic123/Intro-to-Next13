bf5760b918defcec17bb8bb40e711d9ffe447135
Toni Matkovic <toni.matkovic0@gmail.com>
2023-03-13 18:06:09 +0100

    Layouts playground

---
+++ app/contact/layout.tsx
@@ -0,0 +1,8 @@
+export default function ContactLayout({children}: {
+    children: React.ReactNode
+  }) {
+    return <div>
+        <div>contact layout</div>
+        <div>{children}</div>
+    </div>
+}
